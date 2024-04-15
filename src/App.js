3aecde698e7b742f0a93ee20404115fd3e2fdba4
Eugene <zveringa@gmail.com>
2024-04-15 23:25:08 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -4,10 +4,37 @@ import './App.css';
 
 export const App = () => {
 
+  
+  
   const [value, setValue] = useState("");
   const [list, setList] = useState([]);
   const [error, setError] = useState("");
 
+  const isValueValid = value.length >= 3;
+
+  const formatDate = date => {
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    };
+    return new Date(date).toLocaleString('ru-RU', options);
+  };
+
+  const onAddButtonClick = () => {
+    if (isValueValid) {
+      const id = list.length + 1; // уникальный id для нового элемента
+      const createdAt = formatDate(new Date()); // получаем текущую дату и время
+      setList(list => [...list, { id, value, createdAt }]); //используем updater-функцию
+      setValue("");
+      setError("");
+      
+    console.log(list, value, error);//checkup
+    }
+  }
   const onInputButtonClick = () => {
     const promptValue = prompt('Введите значение ');
     if (promptValue && promptValue.length >= 3) {
@@ -31,14 +58,20 @@ export const App = () => {
     <div className={styles['buttons-container']}>
       <button className="button" onClick = {onInputButtonClick} >Ввести новое</button>
       <output>{value}</output>
-      <button className="button" disabled>Добавить в список</button>
+      <button className="button" disabled={!isValueValid} onClick={onAddButtonClick}>Добавить в список</button>
     </div>
     <div className={styles['list-container']}>
       <h2 className={styles['list-heading']}>Список:</h2>
-      <p className={styles['no-margin-text']}>Нет добавленных элементов</p>
-      <ul className="list">
-        <li className={styles['list-item']}>Первый элемент</li>
-      </ul>
+      
+      {list.length > 0 ? (
+
+        <ul className="list">{list.map(item => (
+          <li className={styles['list-item']} key={item.id}>{item.value} - дата и время создания элемента :  ({item.createdAt})</li>
+        ))}
+        </ul>
+      ) : (
+        <p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+      )}
     </div>
   </div>
   );
