89d0dac9e41b4243e76ae921012df5fde2f3358c
Eugene Zveringa <Eugene.Zveringa@tenon-eng.com>
2024-04-18 18:05:31 +0100

    Thursday 18-04

--- src/App.js
+++ src/App.js
@@ -1,80 +1,74 @@
-import styles from 'react';
-import { useState } from 'react';
-import './App.css';
+import styles from './app.module.css';
+import data from './data.json';
+import {useState} from 'react';
 
 export const App = () => {
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+	const onBackButtonClick = () => {
+		setActiveIndex(activeIndex+1);
+	}
+	const onForwardButtonClick = () => {
+		setActiveIndex(activeIndex-1);
+	}
+	const onBeginButtonClick = () => {
+		setActiveIndex(0);
+	}
+// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
 
-  
-  
-  const [value, setValue] = useState("");
-  const [list, setList] = useState([]);
-  const [error, setError] = useState("");
+	const isFirstStep = activeIndex === 0;
+	const isLastStep = steps.data.length - 1;
 
-  const isValueValid = value.length >= 3;
-
-  const formatDate = date => {
-    const options = {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    };
-    return new Date(date).toLocaleString('ru-RU', options);
-  };
-
-  const onAddButtonClick = () => {
-    if (isValueValid) {
-      const id = list.length + 1; // уникальный id для нового элемента
-      const createdAt = formatDate(new Date()); // получаем текущую дату и время
-      setList(list => [...list, { id, value, createdAt }]); //используем updater-функцию
-      setValue("");
-      setError("");
-      
-    console.log(list, value, error);//checkup
-    }
-  }
-  const onInputButtonClick = () => {
-    const promptValue = prompt('Введите значение ');
-    if (promptValue && promptValue.length >= 3) {
-      setValue(promptValue);
-      setError('');
-    } else {
-      setValue('');
-      setError('Введенное значение должно содержать минимум 3 символа!');
-    }
-    
-    console.log(`You entered ${promptValue}`);
-  }
 
 	return (
-    <div className="app">
-    <h1 className={styles['page-heading']}>Ввод значения</h1>
-    <p className={styles['no-margin-text']}>
-      Текущее значение <code>value</code>: "<output className="current-value">{value}</output>"
-    </p>
-    <div className="error">{error}</div>
-    <div className={styles['buttons-container']}>
-      <button className="button" onClick = {onInputButtonClick} >Ввести новое</button>
-      <output>{value}</output>
-      <button className="button" disabled={!isValueValid} onClick={onAddButtonClick}>Добавить в список</button>
-    </div>
-    <div className={styles['list-container']}>
-      <h2 className={styles['list-heading']}>Список:</h2>
-      
-      {list.length > 0 ? (
-
-        <ul className="list">{list.map(item => (
-          <li className={styles['list-item']} key={item.id}>{item.value} - дата и время создания элемента :  ({item.createdAt})</li>
-        ))}
-        </ul>
-      ) : (
-        <p className={styles['no-margin-text']}>Нет добавленных элементов</p>
-      )}
-    </div>
-  </div>
-  );
-
-	
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{/* Для получения активного контента использйте steps и activeIndex */}
+						Контент соответственный шагу. Сейчас активен шаг 3
+					</div>
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						<li className={styles['steps-item'] + ' ' + styles.done}>
+							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
+							<button className={styles['steps-item-button']}>1</button>
+							{/* При клике на кнопку установка выбранного шага в качестве активного */}
+							Шаг 1
+						</li>
+						<li className={styles['steps-item'] + ' ' + styles.done}>
+							<button className={styles['steps-item-button']}>2</button>
+							Шаг 2
+						</li>
+						<li
+							className={
+								styles['steps-item'] +
+								' ' +
+								styles.done +
+								' ' +
+								styles.active
+							}
+						>
+							<button className={styles['steps-item-button']}>3</button>
+							Шаг 3
+						</li>
+						<li className={styles['steps-item']}>
+							<button className={styles['steps-item-button']}>4</button>
+							Шаг 4
+						</li>
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button}>Назад</button>
+						<button className={styles.button}>
+							Далее
+							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
+							{/* Или заменять всю кнопку в зависимости от условия */}
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
